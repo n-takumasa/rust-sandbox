b100943bb486751b22e1c6e452ef91c6f3a83f0c
Takumasa Nakamura <n.takumasa@gmail.com>
2024-07-05 12:10:36 +0900

    bisect: mut + for -> try_fold + ControlFlow

--- src/bisect.rs
+++ src/bisect.rs
@@ -1,34 +1,54 @@
+use std::ops::ControlFlow;
+
+struct Context {
+    a: f64,
+    b: f64,
+    fa: f64,
+    fb: f64,
+}
+
+impl Context {
+    fn new(a: f64, b: f64, fa: f64, fb: f64) -> Self {
+        Self { a, b, fa, fb }
+    }
+}
+
+fn _bisect<F>(f: F, ctx: Context, eps: f64) -> ControlFlow<(f64, f64), Context>
+where
+    F: Fn(f64) -> f64,
+{
+    let m = (ctx.a + ctx.b) / 2.0;
+    let fm = f(m);
+    if f64::abs(ctx.a - ctx.b) / 2.0 < eps || fm.abs() < eps {
+        return ControlFlow::Break((m, fm));
+    }
+    ControlFlow::Continue(match (fm > 0.0) == (ctx.fb > 0.0) {
+        true => Context::new(ctx.a, m, ctx.fa, fm),
+        false => Context::new(m, ctx.b, fm, ctx.fb),
+    })
+}
+
 pub fn bisect<F>(f: F, bound: (f64, f64), max_iter: usize) -> Result<f64, String>
 where
     F: Fn(f64) -> f64,
 {
     let eps = 1e-10;
-    let (mut a, mut b) = bound;
+    let (a, b) = bound;
     if a >= b {
         return Err(format!("a < b, a = {a}, b = {b}"));
     }
-    let (mut fa, mut fb) = (f(a), f(b));
+    let (fa, fb) = (f(a), f(b));
     if (fa > 0.0) == (fb > 0.0) {
         return Err(format!("sign(f(a)) != sign(f(b)), a = {a}, b = {b}"));
     }
 
-    for i in 0..max_iter {
-        if cfg!(test) {
-            println!("{i}: [a, b] = [{a}, {b}]");
-        }
-
-        let m = (a + b) / 2.0;
-        let fm = f(m);
-        if f64::abs(a - b) / 2.0 < eps || fm.abs() < eps {
-            return Ok(m);
-        }
-        (a, b, fa, fb) = match (fm > 0.0) == (fb > 0.0) {
-            true => (a, m, fa, fm),
-            false => (m, b, fm, fb),
-        };
+    match (0..max_iter).try_fold(Context::new(a, b, fa, fb), |ctx, _| _bisect(&f, ctx, eps)) {
+        ControlFlow::Break((m, _fm)) => Ok(m),
+        ControlFlow::Continue(ctx) => Err(format!(
+            "max_iter={max_iter}, [a, b] = [{}, {}]",
+            ctx.a, ctx.b
+        )),
     }
-
-    Err(format!("max_iter={max_iter}, [a, b] = [{a}, {b}]"))
 }
 
 #[cfg(test)]
